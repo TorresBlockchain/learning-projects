86379328e4d3dada367980d63dc329f75033c8b8
Omar Torres <omar.torres25@hotmail.com>
2022-02-09 17:39:50 -0600

    End of day review

--- python-practice-modules/practice_02_008.py
+++ python-practice-modules/practice_02_008.py
@@ -3,3 +3,10 @@
 # updating file from GitHub, going to see if its able to retrieve back to Visual Studio Code
 
 # file updated after committing on website and needed to refresh back at the app to continue working
+
+import math
+
+print(round(2.9))
+print(abs(-2.9))
+
+print(math.ceil(2.2))
