ea275ac416fcdeeb7815fdb9cbcb0dcef25cfe2c
Omar Torres <omar.torres25@hotmail.com>
2022-02-09 16:15:39 -0600

    Commit from Room Office Desktop

---
+++ c_p_p-practice/Practice_001.cpp
@@ -0,0 +1,17 @@
+//Downloaded Visual Studio Code on Room Office on Feb 9th, 2022, already finished reviewing the first C Tutorial on freeCodeCamp. 
+//Already on C++ and will be following along on desktop to build coding/muscle memory
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    string phrase = "Giraffe Academy";
+    string phrasesub;
+    phrasesub = phrase.substr (8,3);
+    cout << phrasesub;
+
+    return 0;
+
+    }
