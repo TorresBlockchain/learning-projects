02d3db963b0c8dacf01095eff54612f037579945
Omar Torres <omar.torres25@hotmail.com>
2022-02-09 17:18:40 -0600

    Update changes

--- python-practice-modules/practice_02_008.py
+++ python-practice-modules/practice_02_008.py
@@ -1,3 +1,5 @@
 # uploading file to see changes
 
-#updating file from GitHub, going to see if its able to retrieve back to Visual Studio Code
+# updating file from GitHub, going to see if its able to retrieve back to Visual Studio Code
+
+# file updated after committing on website and needed to refresh back at the app to continue working
