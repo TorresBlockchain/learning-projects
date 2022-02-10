f2125147afd212764a7910f6c487e38243bf7aa9
Omar Torres <omar.torres25@hotmail.com>
2022-02-10 14:32:04 -0600

    Update on project folders and lecture on Chapter 5

---
+++ python-project-modules/Chapter_03/project_03_01.py
@@ -0,0 +1,11 @@
+# Exercise on having an output of the following
+
+# 1
+# 3
+# 5
+# 7
+# 9
+# 11
+# 13
+# 15
+# We have 8 odd numbers
---
+++ python-project-modules/Chapter_03/project_03_02.py
@@ -0,0 +1,7 @@
+# Exercise on having an output of the following
+
+# 2
+# 4
+# 6
+# 8
+# We have 4 even numbers
