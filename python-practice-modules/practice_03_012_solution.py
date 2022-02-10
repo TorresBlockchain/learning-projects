553adbfb8823e118dec3168d239ed59046fb25ed
Omar Torres <omar.torres25@hotmail.com>
2022-02-10 09:49:41 -0600

    added lessons learned on quiz for chapter 3

--- python-practice-modules/practice_03_012_solution.py
+++ python-practice-modules/practice_03_012_solution.py
@@ -4,3 +4,7 @@ for number in range(1, 10):
         count += 1
         print(number)
 print(f"We have {count} even numbers")
+
+# Lesson learned
+# do not attempt to skip skips while performing a program
+# only use abbrev. methods of writing code, but not to derive the answers due to changes of parameters
