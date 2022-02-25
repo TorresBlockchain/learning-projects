e207d4fde8c0da41ff36a352e5933bf74ed7c737
Omar Torres <omar.torres25@hotmail.com>
2022-02-25 17:46:48 -0600

    Ethereum example updates

---
+++ blockchain-practice/intro-to-smart-contracts/practice_001.sol
@@ -0,0 +1,17 @@
+//storage example from Solidity website
+// A Simple Smart Contract
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract SimpleStorage {
+    uint256 storedData;
+
+    function set(uint256 x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
