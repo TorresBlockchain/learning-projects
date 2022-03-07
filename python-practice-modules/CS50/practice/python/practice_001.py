6cad5749f8e08842b5bcfa548662428dcfac88a5
Omar Torres <omar.torres25@hotmail.com>
2022-03-06 20:35:10 -0600

    CS50 Updates

---
+++ python-practice-modules/CS50/practice/python/practice_001.py
@@ -0,0 +1,17 @@
+# for i in range(3):
+#     print('#')
+
+
+# for i in range(4):
+#     print('?')
+
+
+# for i in range(4):
+#     print('?', end="")
+
+# print("?"*4)
+
+for i in range(3):
+    for j in range(3):
+        print("#", end="")
+    print()  # if one doesn't add the print() at this line, we will still have 9 # but in the same line
---
+++ python-practice-modules/CS50/practice/python/practice_002.py
@@ -0,0 +1,10 @@
+# Examples of scores
+
+scores = [72, 73, 33]
+
+# print("Average: " + str(sum(scores)/len(scores))) # one way of getting the average
+
+# print(f"Average: {str(sum(scores)/len(scores))}") #when using {}, always make sure to add the f in the beginning of the function
+
+average = sum(scores)/len(scores)
+print(f"Average: {average}")
